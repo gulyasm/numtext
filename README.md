7560229b5ea7bfef18d24b69dcd63d3e55820e1d
gulyasm <mgulyas86@gmail.com>
2015-05-16 14:05:21 +0200

    Init commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Number to text converter
+Used for UI.
+
+## Usage
+```go
+import github.com/gulyasm/numtext
+```
+
+```go
+    fmt.Println(numtext.ToText(9231203))
+```
+
+## Format
+Currently supports conversion to:
+ - Million (M)
+ - Billion (B)
+ - Trillion (T)
---
+++ convert.go
@@ -0,0 +1,25 @@
+package numtext
+
+type Magnitude struct {
+	Magnitude int64
+	Symbol    string
+}
+
+func (m *Magnitude) ToText(number int64) string {
+	return fmt.Sprintf("%.1f%s", float64(number)/float64(m.Magnitude), m.Symbol)
+}
+
+func ToText(number int64) string {
+	list := []Magnitude{
+		Magnitude{1000000000000, "T"},
+		Magnitude{1000000000, "B"},
+		Magnitude{1000000, "M"},
+	}
+	for _, m := range list {
+		if m.Magnitude < int64(number) {
+			return m.ToText(int64(number))
+		}
+	}
+	return fmt.Sprintf("%d", number)
+
+}
